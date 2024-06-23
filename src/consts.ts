54bd782775686c3c362322b884a9937ab6e4c1be
mnxmnz <minzidev@gmail.com>
2024-06-24 00:26:53 +0900

    misc: add site copyright to consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Devlog";
+export const SITE_DESCRIPTION =
+  "안녕하세요 👋 개발자 김민지입니다. 개발 관련 지식을 기록하고 공유하고 있습니다.";
+
+const today = new Date();
+export const SITE_COPYRIGHT = `© 2021-${today.getFullYear()} 김민지 All Rights Reserved.`;
